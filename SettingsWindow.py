6e4871bc5266cec186c291578c5567fff9af957d
Anush Varma <anush.formal@outlook.com>
2023-06-26 16:28:19 +0100

    added graph_page to main (need to reformat to a different py file)

--- SettingsWindow.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-
-
-class SettingsWindow(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        label = QtWidgets.QLabel("This is the Settings window", self)
-        layout = QtWidgets.QVBoxLayout(self)
-        layout.addWidget(label)
