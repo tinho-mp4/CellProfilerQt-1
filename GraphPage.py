086ce5a40a173dfe7ff7ca0eb1dbe1a8609800b7
jndmh <92041858+tinho-mp4@users.noreply.github.com>
2023-06-29 12:01:52 +0100

    Removed PCA

--- GraphPage.py
+++ GraphPage.py
@@ -6,7 +6,6 @@
 
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import QSize, QRect, Qt, QTimer, QThread, pyqtSignal
-from sklearn.decomposition import PCA
 
 
 class LoadColumnsThread(QThread):
