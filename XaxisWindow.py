8b685532476531ea95368e008f3fa3e9041284a4
jndmh <92041858+tinho-mp4@users.noreply.github.com>
2023-07-04 23:42:08 +0100

    Refactoring

--- XaxisWindow.py
+++ XaxisWindow.py
@@ -1,9 +1,23 @@
 from PyQt5 import QtCore, QtWidgets, QtGui
 import sys
 
+
 class XaxisWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(XaxisWindow, self).__init__()
+        self.top_combo_box = None
+        self.from_label = None
+        self.top_horizontal_layout = None
+        self.top_vertical_layout = None
+        self.bottom_combobox = None
+        self.select_column_label = None
+        self.bottom_horizontal_layout = None
+        self.bottom_vertical_layout = None
+        self.middle_combobox = None
+        self.select_values_label = None
+        self.middle_vertical_layout = None
+        self.middle_horizontal_layout = None
+        self.gridLayout_2 = None
         self.setObjectName("x_axis_window")
         self.setFixedSize(550, 220)
         self.centralwidget = QtWidgets.QWidget(self)
@@ -16,6 +30,7 @@ class XaxisWindow(QtWidgets.QMainWindow):
     def setupUi(self):
         self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout_2.setObjectName("gridLayout_2")
+
         self.middle_vertical_layout = QtWidgets.QVBoxLayout()
         self.middle_vertical_layout.setObjectName("middle_vertical_layout")
         self.middle_horizontal_layout = QtWidgets.QHBoxLayout()
@@ -72,17 +87,33 @@ class XaxisWindow(QtWidgets.QMainWindow):
 
         self.setCentralWidget(self.centralwidget)
 
-
     def retranslateUi(self):
         translate = QtCore.QCoreApplication.translate
         self.setWindowTitle(translate("MainWindow", "MainWindow"))
         self.select_values_label.setText(translate("MainWindow", "Select values of"))
-        self.select_column_label.setText(translate("MainWindow", "select column"))
+        self.select_column_label.setText(translate("MainWindow", "Select column"))
         self.from_label.setText(translate("MainWindow", "From"))
+
+    def createLabel(self, text):
+        label = QtWidgets.QLabel(self.centralwidget)
+        label.setObjectName("label")
+        label.setText(text)
+        return label
+
+    def createComboBox(self):
+        combobox = QtWidgets.QComboBox(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed
+        )
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(combobox.sizePolicy().hasHeightForWidth())
+        combobox.setSizePolicy(sizePolicy)
+        combobox.setObjectName("combobox")
+        return combobox
 
 
 if __name__ == "__main__":
-
     app = QtWidgets.QApplication(sys.argv)
     x_axis_window = XaxisWindow()
     x_axis_window.show()
