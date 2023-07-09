a184ca9d9bf53ece0d78f8a768723cb57ec8b7fa
jndmh <92041858+tinho-mp4@users.noreply.github.com>
2023-07-10 00:50:13 +0100

    Implemented Graph Canvas

--- GraphPage.py
+++ GraphPage.py
@@ -2,8 +2,20 @@ import matplotlib.pyplot as plt
 from PyQt5 import QtWidgets, QtCore
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-from XaxisWindow import *
+from XaxisWindow import XaxisWindow
 from YaxisWindow import YaxisWindow
+from PyQt5.QtWidgets import QButtonGroup
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+
+class GraphCanvas(FigureCanvas):
+    def __init__(self, fig):
+        super(GraphCanvas, self).__init__(fig)
+        self.setParent(None)
+        self.figure = fig
+        FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
 
 
 class GraphPage(QtWidgets.QWidget):
@@ -26,7 +38,7 @@ class GraphPage(QtWidgets.QWidget):
         self.gridLayout_4 = QtWidgets.QGridLayout(self.graph_grid_frame)
         self.gridLayout_4.setObjectName("gridLayout_4")
 
-        # left side
+        # Left Side
         self.vertical_layout_graph_left = QtWidgets.QVBoxLayout()
         self.vertical_layout_graph_left.setObjectName("verticalLayout_4")
 
@@ -50,25 +62,25 @@ class GraphPage(QtWidgets.QWidget):
         self.y_axis_button.setText("Y-Axis")
         self.vertical_layout_graph_left.addWidget(self.y_axis_button)
 
-        # right side
-
+        # Right Side
         self.vertical_layout_graph_right = QtWidgets.QVBoxLayout()
         self.vertical_layout_graph_right.setObjectName("vertical_layout_graph_right")
 
-        # search bar / check box area
+        # Search Bar / Check Box Area
         self.check_all_horizontal_layout_2 = QtWidgets.QHBoxLayout()
         self.check_all_horizontal_layout_2.setObjectName("check_all_horizontal_layout_2")
         self.check_all_box_2 = QtWidgets.QCheckBox(self.graph_grid_frame)
         self.check_all_box_2.setSizeIncrement(QtCore.QSize(0, 0))
         self.check_all_box_2.setObjectName("Check_all_box_2")
         self.check_all_box_2.setText("Check All")
+        self.check_all_box_2.setChecked(True)
         self.check_all_horizontal_layout_2.addWidget(self.check_all_box_2)
         self.searchbar_2 = QtWidgets.QLineEdit(self.graph_grid_frame)
         self.searchbar_2.setObjectName("searchbar_2")
         self.check_all_horizontal_layout_2.addWidget(self.searchbar_2)
         self.vertical_layout_graph_right.addLayout(self.check_all_horizontal_layout_2)
 
-        # scroll area
+        # Scroll Area
         self.scrollArea_2 = QtWidgets.QScrollArea(self.graph_grid_frame)
         self.scrollArea_2.setWidgetResizable(True)
         self.scrollArea_2.setObjectName("scrollArea_2")
@@ -85,13 +97,13 @@ class GraphPage(QtWidgets.QWidget):
         spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontal_layout_generate_button.addItem(spacerItem2)
 
-        # generate button
+        # Generate Button
         self.generate_graph = QtWidgets.QPushButton(self.graph_grid_frame)
         self.generate_graph.setObjectName("generate_graph")
         self.generate_graph.setText("Generate")
         self.horizontal_layout_generate_button.addWidget(self.generate_graph)
 
-        # left side PCA.. Options
+        # Left Side PCA.. Options
         self.graph_options = QtWidgets.QGridLayout()
         self.graph_options.setObjectName("graph_options")
 
@@ -115,7 +127,7 @@ class GraphPage(QtWidgets.QWidget):
         self.LDA_radio_button.setText("LDA")
         self.graph_options.addWidget(self.LDA_radio_button, 1, 1, 1, 1)
 
-        # spacers
+        # Spacers
         spacerItem3 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         spacerItem4 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
@@ -124,21 +136,45 @@ class GraphPage(QtWidgets.QWidget):
         self.graph_options.addItem(spacerItem3, 2, 1, 1, 1)
         self.graph_options.addItem(spacerItem4, 2, 0, 1, 1)
 
-        # layouts
+        self.graph_type_button_group = QButtonGroup(self)
+        self.dimensionality_button_group = QButtonGroup(self)
+
+        self.graph_type_button_group.addButton(self.scatter_plot_radio)
+        self.graph_type_button_group.addButton(self.bar_graph_radio)
+
+        self.dimensionality_button_group.addButton(self.PCA_radio_button)
+        self.dimensionality_button_group.addButton(self.tSNE_radio_button)
+        self.dimensionality_button_group.addButton(self.UMAP_radio_button)
+        self.dimensionality_button_group.addButton(self.LDA_radio_button)
+
+        # Layouts
         self.gridLayout_4.addLayout(self.graph_options, 0, 2, 1, 1)
         self.gridLayout_7.addWidget(self.graph_grid_frame, 0, 0, 1, 1)
         self.vertical_layout_graph_right.addLayout(self.horizontal_layout_generate_button)
         self.gridLayout_4.addLayout(self.vertical_layout_graph_right, 0, 3, 1, 1)
         self.gridLayout_4.addLayout(self.vertical_layout_graph_left, 0, 0, 1, 1)
 
-        # Connect the signal handlers
+        # Signal Handlers
         self.check_all_box_2.stateChanged.connect(self.toggleAllCheckboxes)
         self.searchbar_2.textChanged.connect(self.handleSearch)
-        self.generate_graph.clicked.connect(self.generate_graph_handler)
-        self.x_axis_button.clicked.connect(lambda: self.x_axis_handler())
-        self.y_axis_button.clicked.connect(lambda: self.y_axis_handler())
+        self.generate_graph.clicked.connect(self.update_graph)
+        self.x_axis_button.clicked.connect(self.x_axis_handler)
+        self.y_axis_button.clicked.connect(self.y_axis_handler)
 
         self.checkboxes = []
+
+    def toggleGraphType(self):
+        self.PCA_radio_button.setEnabled(self.scatter_plot_radio.isChecked())
+        self.tSNE_radio_button.setEnabled(self.scatter_plot_radio.isChecked())
+        self.UMAP_radio_button.setEnabled(self.scatter_plot_radio.isChecked())
+        self.LDA_radio_button.setEnabled(self.scatter_plot_radio.isChecked())
+
+    def toggleDimensionalityScaling(self):
+        self.scatter_plot_radio.setEnabled(
+            self.PCA_radio_button.isChecked()
+            or self.tSNE_radio_button.isChecked()
+            or self.UMAP_radio_button.isChecked()
+            or self.LDA_radio_button.isChecked())
 
     def display_data_columns(self, columns):
         for i in range(self.gridLayout_5.count()):
@@ -159,8 +195,9 @@ class GraphPage(QtWidgets.QWidget):
 
     def handleSearch(self, text):
         self.search_text = text
-        self.timer.stop()
-        self.timer.start(500)
+        self.search_timer.stop()
+        if len(self.search_text) > 2:
+            self.search_timer.start(1000)
 
     def perform_search(self):
         text = self.search_text.lower()
@@ -183,12 +220,10 @@ class GraphPage(QtWidgets.QWidget):
         elif self.UMAP_radio_button.isChecked():
             dimensionality_reduction = "UMAP"
 
-        # Perform the graph generation using the selected columns and options
         self.generate_graph_with_columns(selected_columns, graph_type, dimensionality_reduction)
 
     def generate_graph_with_columns(self, columns, graph_type, dimensionality_reduction):
-        # Perform the graph generation using the selected columns and options
-        data = self.data[columns]  # Retrieve the selected columns from the loaded data
+        data = self.data_frame[columns]
 
         if dimensionality_reduction == "PCA":
             reducer = PCA(n_components=2)
@@ -201,19 +236,39 @@ class GraphPage(QtWidgets.QWidget):
 
         reduced_data = reducer.fit_transform(data)
 
+        self.graph_canvas.axes.clear()
+
         if graph_type == "scatter":
-            plt.scatter(reduced_data[:, 0], reduced_data[:, 1])
-            plt.xlabel("Component 1")
-            plt.ylabel("Component 2")
-            plt.title("Scatter Plot")
-            plt.show()
+            self.graph_canvas.axes.scatter(reduced_data[:, 0], reduced_data[:, 1])
+            self.graph_canvas.axes.set_xlabel("Component 1")
+            self.graph_canvas.axes.set_ylabel("Component 2")
+            self.graph_canvas.axes.set_title("Scatter Plot")
+
         elif graph_type == "bar":
-            plt.bar(range(len(columns)), reduced_data)
-            plt.xlabel("Columns")
-            plt.ylabel("Values")
-            plt.title("Bar Chart")
-            plt.xticks(range(len(columns)), columns, rotation=90)
-            plt.show()
+            self.graph_canvas.axes.bar(range(len(columns)), reduced_data)
+            self.graph_canvas.axes.set_xlabel("Columns")
+            self.graph_canvas.axes.set_ylabel("Values")
+            self.graph_canvas.axes.set_title("Bar Chart")
+            self.graph_canvas.axes.set_xticks(range(len(columns)))
+            self.graph_canvas.axes.set_xticklabels(columns, rotation=90)
+
+        self.graph_canvas.draw()
+
+    def update_graph(self):
+        x_columns = self.x_axis_window.get_x_axis_columns()
+        y_columns = self.y_axis_window.get_y_axis_columns()
+
+        if x_columns and y_columns:
+            graph_type = "scatter" if self.scatter_plot_radio.isChecked() else "bar"
+            dimensionality_reduction = ""
+            if self.PCA_radio_button.isChecked():
+                dimensionality_reduction = "PCA"
+            elif self.tSNE_radio_button.isChecked():
+                dimensionality_reduction = "t-SNE"
+            elif self.UMAP_radio_button.isChecked():
+                dimensionality_reduction = "UMAP"
+
+            self.generate_graph_with_columns(x_columns, graph_type, dimensionality_reduction)
 
     def x_axis_handler(self):
         self.x_axis_window = XaxisWindow()
@@ -227,8 +282,8 @@ class GraphPage(QtWidgets.QWidget):
         self.y_axis_window = YaxisWindow()
         self.y_axis_window.set_table_data_frame(self.data_frame)
         for column in self.data_columns:
-            self.y_axis_window.top_combo_box.addItemToComboBox(column)
-            self.y_axis_window.bottom_combobox.addItemToComboBox(column)
+            self.y_axis_window.top_combo_box.addItem(column)
+            self.y_axis_window.bottom_combobox.addItem(column)
         self.y_axis_window.show()
 
     def set_table_data_columns(self, columns):
